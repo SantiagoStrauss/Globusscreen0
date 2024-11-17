d56f8410c3071e1d7f25b1cf9d070545beb5684f
SantiagoStrauss <ds.ramos@uniandes.edu.co>
2024-11-17 15:20:30 -0500

    s9

--- app/actions.ts
+++ app/actions.ts
@@ -5,10 +5,6 @@ import { createClient } from "@/utils/supabase/server";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
-export const config = {
-  runtime: 'edge',
-};
-
 export const signUpAction = async (formData: FormData) => {
   const email = formData.get("email")?.toString();
   const password = formData.get("password")?.toString();
